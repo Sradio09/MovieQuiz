6d6368877ca5dc39258ab0978972c87d9a5d87e2
Sradio <sradio09@yandex.ru>
2025-04-26 22:00:34 +0300

    Выполнение 5ого спринта

--- MovieQuiz/Presentation/Services/QuestionFactory.swift
+++ MovieQuiz/Presentation/Services/QuestionFactory.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-struct QuizQuestionMock {
-    static let questions: [QuizQuestion] = [
+class QuestionFactory: QuestionFactoryProtocol {
+    weak var delegate: QuestionFactoryDelegate?
+
+    func setup(delegate: QuestionFactoryDelegate) {
+            self.delegate = delegate
+        }
+    private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
             text: "Рейтинг этого фильма больше чем 6?",
@@ -43,4 +48,15 @@ struct QuizQuestionMock {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
+    func requestNextQuestion() {
+        guard let index = (0..<questions.count).randomElement() else {
+            delegate?.didReceiveNextQuestion(question: nil)
+            return
+        }
+
+        let question = questions[safe: index]
+        delegate?.didReceiveNextQuestion(question: question)
+    }
 }
+
+
---
+++ MovieQuiz/Presentation/Services/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  MovieQuiz
+//
+//  Created by Smirnov Michael on 20.04.2025.
+//
+

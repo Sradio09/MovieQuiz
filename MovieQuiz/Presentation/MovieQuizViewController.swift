12c65d373494e6d36bca6f9b77f260d768b6e4de
Sradio <sradio09@yandex.ru>
2025-05-06 23:31:03 +0300

    Выполнение 7 спринта

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
+final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol {
+    
+    // MARK: - Outlets
     
     @IBOutlet weak private var imageView: UIImageView!
     @IBOutlet weak private var counterLabel: UILabel!
@@ -9,51 +11,34 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     @IBOutlet weak private var noButton: UIButton!
     @IBOutlet private var activityIndicator: UIActivityIndicatorView!
     
-    private let presenter = MovieQuizPresenter()
-    private var questionFactory: QuestionFactoryProtocol?
-    private var currentQuestion: QuizQuestion?
-    private var statisticService: StatisticServiceProtocol = StatisticService()
-    private var correctAnswers = 0
-    private var alertPresenter: AlertPresenter?
+    // MARK: - Properties
+    
+    private var presenter: MovieQuizPresenter!
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        presenter.viewController = self
+        presenter = MovieQuizPresenter(viewController: self)
+        presenter.alertPresenter = AlertPresenter(viewController: self)
         
-        alertPresenter = AlertPresenter(viewController: self)
-        
+        setupUI()
+        showLoadingIndicator()
+    }
+    
+    // MARK: - UI Setup
+    
+    private func setupUI() {
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.cornerRadius = 20
         imageView.clipsToBounds = true
-        
-        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(), delegate: self)
-        statisticService = StatisticService()
-        showLoadingIndicator()
-        questionFactory?.loadData()
-    }
-    func didReceiveNextQuestion(question: QuizQuestion?){
-        guard let question else { return }
-        
-        currentQuestion = question
-        let viewModel = presenter.convert(model: question)
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.show(quiz: viewModel)
-        }
     }
     
-    func didLoadDataFromServer() {
-        activityIndicator.isHidden = true
-        questionFactory?.requestNextQuestion()
-    }
+    // MARK: - Public UI Methods
     
-    func didFailToLoadData(with error: Error) {
-        showNetworkError(message: error.localizedDescription)
-    }
-    
-    private func show(quiz step: QuizStepModel) {
+    func show(quiz step: QuizStepModel) {
         imageView.layer.borderColor = UIColor.clear.cgColor
         imageView.image = step.image
         textLabel.text = step.question
@@ -63,110 +48,63 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         noButton.isEnabled = true
     }
     
-    private func show(quiz result: QuizResultsModel) {
-        let alertModel = AlertModel (
+    func show(quiz result: QuizResultsModel) {
+        let alertModel = AlertModel(
             title: result.title,
             message: result.text,
             buttonText: result.buttonText,
             completion: { [weak self] in
-                guard let self = self else {return}
-                self.presenter.resetQuestionIndex()
-                self.correctAnswers = 0
-                self.questionFactory?.requestNextQuestion()
+                guard let self = self else { return }
+                self.presenter.restartGame()
             },
             accessibilityIdentifier: "Model results"
         )
-        alertPresenter?.show(alert: alertModel)
+        presenter.alertPresenter?.show(alert: alertModel)
     }
     
-    private func showLoadingIndicator() {
+    func showLoadingIndicator() {
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
     }
     
-    private func showNetworkError(message: String) {
-        
+    func hideLoadingIndicator() {
+        activityIndicator.isHidden = true
+    }
+    
+    func showNetworkError(message: String) {
         let alertModel = AlertModel(
             title: "Ошибка",
             message: message,
             buttonText: "Попробовать ещё раз",
             completion: { [weak self] in
                 guard let self = self else { return }
-                
-                self.presenter.resetQuestionIndex()
-                self.correctAnswers = 0
-                
+                self.presenter.restartGame()
                 self.showLoadingIndicator()
-                self.questionFactory?.loadData()
             },
             accessibilityIdentifier: "networkErrorAlert"
         )
-        
-        alertPresenter?.show(alert: alertModel)
+        presenter.alertPresenter?.show(alert: alertModel)
     }
     
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+        imageView.layer.borderColor = isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+    }
+    
+    func disableAnswerButtons() {
+        yesButton.isEnabled = false
+        noButton.isEnabled = false
+    }
+    
+    // MARK: - Actions
+    
     @IBAction private func yesButtonTapped(_ sender: UIButton) {
-        presenter.currentQuestion = currentQuestion
+        disableAnswerButtons()
         presenter.yesButtonTapped()
     }
     
     @IBAction private func noButtonTapped(_ sender: UIButton) {
-        presenter.currentQuestion = currentQuestion
+        disableAnswerButtons()
         presenter.noButtonTapped()
-    }
-    
-    func showAnswerResult(isCorrect: Bool) {
-        
-        yesButton.isEnabled = false
-        noButton.isEnabled = false
-        
-        if isCorrect {
-            correctAnswers += 1
-        }
-        
-        imageView.layer.borderColor = isCorrect
-        ? UIColor.customGreen?.cgColor
-        : UIColor.customRed?.cgColor
-        
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            guard let self = self else { return }
-            self.showNextQuestionOrResults()
-        }
-    }
-    
-    private func showNextQuestionOrResults() {
-        if presenter.isLastQuestion() {
-            statisticService.store(correct: correctAnswers, total: presenter.questionsAmount)
-            let bestGame = statisticService.bestGame
-            let gamesCount = statisticService.gamesCount
-            let formattedDate = DateFormatter.localizedString(
-                from: bestGame.date,
-                dateStyle: .short,
-                timeStyle: .short
-            )
-            let message = """
-        Ваш результат: \(correctAnswers)/\(presenter.questionsAmount)
-        Количество сыграных квизов: \(gamesCount)
-        Рекорд: \(bestGame.correct)/\(bestGame.total) (\(formattedDate))
-        Средняя точность: \(String(format: "%.2f", statisticService.totalAccuracy))%
-        """
-            let alertModel = AlertModel(
-                title: "Этот раунд окончен!",
-                message: message,
-                buttonText: "Сыграть ещё раз",
-                completion: { [weak self] in
-                    guard let self = self else { return }
-                    self.presenter.resetQuestionIndex()
-                    self.correctAnswers = 0
-                    self.questionFactory?.requestNextQuestion()
-                },accessibilityIdentifier: "Game results"
-            )
-            alertPresenter?.show(alert: alertModel)
-        } else {
-            presenter.switchToNextQuestion()
-            self.questionFactory?.requestNextQuestion()
-        }
     }
 }
 
--- MovieQuiz/Presentation/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Presentation/Services/QuestionFactoryDelegate.swift
@@ -1,4 +1,5 @@
 import Foundation
+
 protocol QuestionFactoryDelegate: AnyObject {
     func didReceiveNextQuestion(question: QuizQuestion?)
     func didLoadDataFromServer()
